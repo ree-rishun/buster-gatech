c9e1c47e3fc0b93fabbd3ed0d8db41e737413148
ree-rishun <ree0432@gmail.com>
2020-12-14 06:55:08 +0900

    フォームのバリデーション

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,22 +3,44 @@
     <h2>ユーザ登録</h2>
 
     <label>
-      <input type="text" v-model="user.lastName" placeholder="苗字">
+      <input
+        type="text"
+        v-model="user.lastName"
+        :class="errors.lastName.error ? 'error_input' : ''"
+        placeholder="苗字">
     </label>
     <label>
-      <input type="text" v-model="user.firstName" placeholder="名前">
+      <input
+        type="text"
+        v-model="user.firstName"
+        :class="errors.firstName.error ? 'error_input' : ''"
+        placeholder="名前">
     </label>
     <label>
-      <input type="email" v-model="user.email" placeholder="メールアドレス">
+      <input
+        type="email"
+        v-model="user.email"
+        :class="errors.email.error ? 'error_input' : ''"
+        placeholder="メールアドレス">
     </label>
     <label>
-      <input type="password" v-model="user.password" placeholder="パスワード">
+      <input
+        type="password"
+        v-model="user.password"
+        :class="errors.password.error ? 'error_input' : ''"
+        placeholder="パスワード（6文字以上）">
     </label>
     <label>
-      <input type="tel" v-model="user.tel" placeholder="電話番号（任意）">
+      <input
+        type="tel"
+        v-model="user.tel"
+        placeholder="電話番号（任意）">
     </label>
     <label>
-      <input type="text" v-model="user.address" placeholder="住所（任意）">
+      <input
+        type="text"
+        v-model="user.address"
+        placeholder="住所（任意）">
     </label>
     <label>
       <input type="checkbox" v-model="user.merumaga"> メルマガに登録します。
@@ -29,10 +51,16 @@
 
     <label>
       <button
+        v-if="user.term"
         class="submit"
         @click="registerSubmit">
         登録
       </button>
+      <span
+        v-else
+        class="submit_disable">
+        登録
+      </span>
     </label>
 
     <label>
@@ -63,14 +91,31 @@
           address: '',
           term: false,
           merumaga: true
-        }
+        },
+        errors: {
+          firstName: {
+            error: false
+          },
+          lastName: {
+            error: false
+          },
+          email: {
+            error: false
+          },
+          password: {
+            error: false
+          }
+        },
       }
     },
     methods: {
       registerSubmit () { // 会員登録処理
 
-        // パスワードが6文字以上か？
-        // e-mailのフォーマット確認
+        // 入力値チェック
+        if (!this.checkForm()) {
+          console.log('error')
+          return
+        }
 
         // Authenticationへの登録
         firebase
@@ -122,6 +167,27 @@
             console.log('ERROR CODE : ' + errorCode)
             console.log(errorMessage)
           })
+      },
+      checkForm () {
+        // 空の判定
+        this.errors.firstName.error = this.user.firstName === ''
+        this.errors.lastName.error = this.user.lastName === ''
+        this.errors.email.error = this.user.email === ''
+        this.errors.password.error = this.user.password === ''
+
+        // パスワードの文字数が6文字以下の判定
+        if (this.user.password.length <= 6) {
+          this.errors.password.error = false
+        }
+
+        // エラーが存在する場合は false を返す
+        for (let value in this.errors) {
+          if (this.errors[value].error) {
+            return false
+          }
+        }
+
+        return true
       }
     }
   }
@@ -132,6 +198,7 @@
     text-align: center;
   }
 
+  // 入力フォームスタイル
   label{
     display: block;
     text-align: center;
@@ -149,16 +216,26 @@
         border: none;
         border-bottom: solid 1px #0080ff;
       }
+
+      // エラー時のスタイル
+      &.error_input{
+        border-bottom: solid 1px #ff536a;
+      }
     }
   }
 
   // 送信ボタン
-  .submit{
+  .submit, .submit_disable{
+    display: inline-block;
     height: 35px;
+    line-height: 35px;
     width: 80px;
     border-radius: 35px;
     background: #fcdc4a;
     color: #111111;
     font-weight: bold;
+  }
+  .submit_disable{
+    background: #9f9f9f;
   }
 </style>
